5e897bcf57eaa4c39cf19e2a60a006bba489f3da
usha Ahiraw <ushaahirwarahirwar@8gamil.com>
2025-02-21 19:08:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// id element  
+let a = document.getElementById("demo")
+console.log(a);
+  a.textContent="hello from usha";
+a.innerHTML ="<i>change by Asmita !!!....</i>"
+
+
+
+// className element
+let b = document.getElementsByClassName("demo1");
+
+
+// tagName element
+let c =document.getElementsByTagName('h3')
+ c[1].innerHTML="cybrom";
+ c[2].innerHTML="cybrom";
+console.log(c)
+
